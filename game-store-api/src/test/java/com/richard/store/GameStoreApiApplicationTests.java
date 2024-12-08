b3c8d34df83e1ea053c77cef02b77d5ebc399a04
d_nnaji <richard.nnaji013@gmail.com>
2024-12-08 20:44:08 +0100

    initializing the backend project

---
+++ game-store-api/src/test/java/com/richard/store/GameStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richard.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
